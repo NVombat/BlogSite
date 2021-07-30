ce22120043f313b29f2f4016af349cd217ff6132
NVombat <nvombatkere@gmail.com>
2021-07-30 23:42:35 +0530

    Users & User Profiles

--- django_blog/core/urls.py
+++ django_blog/core/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as users_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -27,3 +29,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name = 'logout.html'), name='logout'),
     path('', include('blog.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django_blog/users/models.py
+++ django_blog/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.base import Model
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='defualt.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
