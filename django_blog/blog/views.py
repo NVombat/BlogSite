cee1fd00db2b07ba4f561c3e592b413e3012de2b
NVombat <nvombatkere@gmail.com>
2021-07-31 23:01:56 +0530

    Post Create, Update, Delete & View Change

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,53 @@
 from django.shortcuts import render
 from .models import post
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+class PostListView(ListView):
+    model = post
+    template_name = "home.html"
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = post
+    template_name = "posts.html"
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = post
+    fields=['title', 'content']
+    template_name = "post_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = post
+    fields=['title', 'content']
+    template_name = "post_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = post
+    template_name = "post_delete.html"
+
+    success_url = "/"
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def home(request):
     context = {
